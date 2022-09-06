ce0629da2b65d08493f696c47ee08d5d0e3d7e26
The Boy <52394592+RedBlazerFlame@users.noreply.github.com>
2022-09-06 13:36:13 +0800

    Update Q1Ex2.java
    Added main menu and settings change
    
    TODO: add y/n to handle quitting at the end of certain subroutines, handle out of bounds guessing

--- Q1Ex2/Q1Ex2/src/q1ex2/Q1Ex2.java
+++ Q1Ex2/Q1Ex2/src/q1ex2/Q1Ex2.java
@@ -1,4 +1,4 @@
-package q1ex2;
+// package q1ex2;
 
 import java.util.Scanner;
 
@@ -33,26 +33,99 @@ class InputUtils {
     }
 }
 
+class Settings {
+    // Attributes
+    public int lowerBound;
+    public int upperBound;
+    private int numGuesses;
+    
+    // Constructor
+    public Settings(int a_lowerBound, int a_upperBound, int a_numGuesses) {
+        lowerBound = a_lowerBound;
+        upperBound = a_upperBound;
+        numGuesses = a_numGuesses;
+    }
+    
+    // Getters and Setters
+    public int getNumGuesses() {
+        return numGuesses;
+    }
+    
+    public Settings setNumGuesses(int a_numGuesses) {
+        if(a_numGuesses > 0) {
+            numGuesses = a_numGuesses;
+        }
+        return this;
+    }
+}
+
 class Subroutines {
-    public static void startGame(Scanner sc, int maxNum, int maxGuesses) {
+    public static void showMenu(Scanner sc, Settings settings) {
+        // Showing options
+        System.out.println("Welcome to Higher or Lower! What will you do?\n-Start game\n-Change settings\n-End application");
+        String action = sc.nextLine();
+        String actionLowerCase = action.toLowerCase();
+        
+        if(actionLowerCase.equals("start game")) {
+            startGame(sc, settings);
+        } else if(actionLowerCase.equals("change settings")) {
+            changeSettings(sc, settings);
+        } else if(actionLowerCase.equals("end application")) {
+            endApplication();
+        } else {
+            System.out.println("Invalid Action");
+        }
+        showMenu(sc, settings);
+    }
+    
+    public static void startGame(Scanner sc, Settings settings) {
         // Generate a random number in the range [1, 10]
-        int random = Random.randInt(1, maxNum);
+        int random = Random.randInt(settings.lowerBound, settings.upperBound);
         
         // Declaring a guess variable to store the user's guess
         int guess = 0;
-        int guessesLeft = maxGuesses;
+        int guessesLeft = settings.getNumGuesses();
                 
         do {
             System.out.printf("You have %d guess%s left. What is your guess? %n", guessesLeft, (guessesLeft > 1 ? "es" : ""));
             guess = InputUtils.nextInt(sc);
-            if(guess < random) {
-                System.out.print("Guess higher! ");
-            } else {
-                System.out.print("Guess lower! ");
-            }
             
             guessesLeft --;
+            
+            if(guessesLeft == 0) break;
+            
+            if(guess < random) {
+                System.out.print("Guess higher! ");
+            } else if(guess > random) {
+                System.out.print("Guess lower! ");
+            }
         } while(guess != random);
+        
+        // Concluding the game
+        if(guess == random) {
+            System.out.println("You got it!");
+        } else {
+            System.out.println("You lost...");
+        }
+    }
+    
+    public static void changeSettings(Scanner sc, Settings settings) {
+        // Setting the Lower Bound
+        System.out.print("What is the lower limit of the random number? ");
+        settings.lowerBound = InputUtils.nextInt(sc);
+        
+        // Setting the Upper Bound
+        System.out.print("What is the upper limit of the random number? ");
+        settings.upperBound = InputUtils.nextInt(sc);
+        
+        // Setting the number of guesses
+        System.out.print("How many guesses are allowed? ");
+        settings.setNumGuesses(InputUtils.nextInt(sc));
+    }
+    
+    public static void endApplication() {
+        System.out.println("Thank you for playing!");
+        System.exit(0);
     }
 }
 
@@ -64,6 +137,11 @@ public class Q1Ex2 {
     public static void main(String[] args) {
         // Declaring a scanner object
         Scanner sc = new Scanner(System.in);
+        
+        // Declaring the settings object
+        Settings settings = new Settings(1, 10, 3);
+        
+        Subroutines.showMenu(sc, settings);
     }
 
 }
